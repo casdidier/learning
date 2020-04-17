c10739710369bd5685b3cb2f8f9a188b1c250f22
casdidier <cheung.didier@gmail.com>
2020-04-17 12:30:50 +0200

    clean

--- justjs/strictEquals.js
+++ justjs/strictEquals.js
@@ -1,6 +1,7 @@
 // Write a function called strictEquals(a, b)
 //  that returns the same value as a === b.
 //  Your implementation must not  use the === or !== operators.
+// https://repl.it/@DidierCAS/strictEquals-test
 
 // Like a === b
 function strictEquals(a, b) {
