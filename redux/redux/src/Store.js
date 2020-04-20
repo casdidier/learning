ff9966937f9d3d51ef7eb0f0710b44a337d7034d
casdidier <cheung.didier@gmail.com>
2020-04-20 19:21:29 +0200

    typical imports needed for redux-react app

--- redux/redux/src/Store.js
+++ redux/redux/src/Store.js
@@ -1,6 +1,24 @@
-import Redux from 'react-redux';
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { Provider, connect } from 'react-redux'
+// import { createStore, combineReducers, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk'
 
-// Redux:
+// import rootReducer from './redux/reducers'
+// import App from './components/App'
+
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunk)
+// );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App/>
+//   </Provider>,
+//   document.getElementById('root')
+// );
+Redux: 
 const ADD = 'ADD';
 
 const addMessage = (message) => {
