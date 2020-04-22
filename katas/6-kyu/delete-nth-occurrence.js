f72da96899dbe321ffa85c486d3b991f4764b0cf
casdidier <cheung.didier@gmail.com>
2020-04-22 08:19:42 +0200

    improvement

--- katas/6-kyu/delete-nth-occurrence.js
+++ katas/6-kyu/delete-nth-occurrence.js
@@ -1,21 +1,32 @@
 // https://www.codewars.com/kata/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times/train/javascript
 
-const deleteNth = (arr, n) => {
-  const counter = {};
+// const deleteNth = (arr, n) => {
+//   const counter = {};
 
-  const arrCounter = arr.map((x) => {
-    return counter[x] ? counter[x] += 1 : counter[x] = 1;
+//   const arrCounter = arr.map((x) => {
+//     return counter[x] ? counter[x] += 1 : counter[x] = 1;
+//   });
+
+//   return arr.map((x, i) => {
+//     return arrCounter[i] <= n ? x : null;
+//   })
+//     .filter(x => x !== null);
+// };
+
+function deleteNth(arr, n) {
+  const counts = {};
+  const result = [];
+
+  arr.forEach((currentNumber) => {
+    if (!counts[currentNumber] || counts[currentNumber] < n) {
+      result.push(currentNumber);
+      counts[currentNumber] = counts[currentNumber] || 0;
+      counts[currentNumber]++;
+    }
   });
 
-  return arr.map((x, i) => {
-    return arrCounter[i] <= n ? x : null;
-  })
-    .filter(x => x !== null);
-};
-
-// function deleteNth(arr, n) {
-
-// }
+  return result;
+}
 
 
 console.log(deleteNth([20, 37, 20, 21], 1), [20, 37, 21]);
