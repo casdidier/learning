9568bf2c8e294d675010bf95ba95019ed7e69e6f
casdidier <cheung.didier@gmail.com>
2020-04-24 08:44:32 +0200

    cleaner

--- katas/6-kyu/title-case.js
+++ katas/6-kyu/title-case.js
@@ -35,29 +35,45 @@ function capitalize(word) {
   return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
+// function titleCase(title, minorWords = '') {
+//   if (title.trim() === '') return '';
+//   // create structure to use for checking if a given word is a minor word
+//   // split minorWords on spaces into a new array
+//   // a place to store the minor words structure (object)
+//   const checkMinorWords = minorWords
+//     .toLowerCase()
+//     .split(' ')
+//     .reduce((words, word) => {
+//       // set property on the minor words structure with the given word and a value of true
+//       words[word] = true;
+//       return words;
+//     }, {});
+
+//   // split the title on spaces into a new array
+//   // a place to store the final title
+//   // iterate over array of words
+//   return title
+//     .toLowerCase()
+//     .split(' ')
+//     .map((word, i) => {
+//       if (i === 0) return capitalize(word);
+//       if (checkMinorWords[word]) return word;
+//       return capitalize(word);
+//     }).join(' ');
+// }
+
 function titleCase(title, minorWords = '') {
   if (title.trim() === '') return '';
-  // create structure to use for checking if a given word is a minor word
-  // split minorWords on spaces into a new array
-  // a place to store the minor words structure (object)
+
   const checkMinorWords = minorWords
     .toLowerCase()
-    .split(' ')
-    .reduce((words, word) => {
-      // set property on the minor words structure with the given word and a value of true
-      words[word] = true;
-      return words;
-    }, {});
+    .split(' ');
 
-  // split the title on spaces into a new array
-  // a place to store the final title
-  // iterate over array of words
   return title
     .toLowerCase()
     .split(' ')
     .map((word, i) => {
-      if (i === 0) return capitalize(word);
-      if (checkMinorWords[word]) return word;
+      if (i && checkMinorWords.includes(word)) return word;
       return capitalize(word);
     }).join(' ');
 }
