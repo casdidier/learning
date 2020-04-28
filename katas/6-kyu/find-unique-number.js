94e33507e78635b8f6c75b50a50a2112d194bcd7
casdidier <cheung.didier@gmail.com>
2020-04-28 05:33:18 +0200

    improvement

--- katas/6-kyu/find-unique-number.js
+++ katas/6-kyu/find-unique-number.js
@@ -1,21 +1,25 @@
 // https://www.codewars.com/kata/find-the-unique-number-1/train/javascript
 
+// function findUniq(arr) {
+//   const counter = {};
+
+//   arr.map((x) => {
+//     if (!counter[x]) { counter[x] = 1; } else {
+//       counter[x] += 1;
+//     }
+//   });
+
+//   for (const key in counter) {
+//     if (counter[key] === 1) {
+//       return Number(key);
+//     }
+//   }
+// }
+
+// more functionnal
 function findUniq(arr) {
-  const counter = {};
-
-  arr.map((x) => {
-    if (!counter[x]) { counter[x] = 1; } else {
-      counter[x] += 1;
-    }
-  });
-
-  for (const key in counter) {
-    if (counter[key] === 1) {
-      return Number(key);
-    }
-  }
+  return +arr.filter((value) => { return arr.indexOf(value) == arr.lastIndexOf(value); });
 }
-
 
 console.log(findUniq([0, 1, 0]), 1);
 console.log(findUniq([1, 1, 1, 2, 1, 1]), 2);
