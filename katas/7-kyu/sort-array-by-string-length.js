401a830c4672881a407935ff1ad1fdb653467524
casdidier <cheung.didier@gmail.com>
2020-05-07 08:05:43 +0200

    kata changed

--- katas/7-kyu/sort-array-by-string-length.js
+++ katas/7-kyu/sort-array-by-string-length.js
@@ -1,9 +1,22 @@
 // https://www.codewars.com/kata/sort-array-by-string-length/train/javascript
 
 function sortByLength(array) {
-  // Return an array containing the same strings, ordered from shortest to longest
-  return array.sort((a, b) => a.length > b.length);
+  array.sort((a, b) => {
+    if (a.length > b.length) {
+      return 1;
+    }
+    if (a.length < b.length) {
+      return -1;
+    }
+    return 0;
+  });
+  return array;
 }
+
+// function sortByLength(array) {
+//   // Return an array containing the same strings, ordered from shortest to longest
+//   return array.sort((a, b) => a.length > b.length);
+// }
 
 console.log(sortByLength(['Beg', 'Life', 'I', 'To']), ['I', 'To', 'Beg', 'Life']);
 console.log(sortByLength(['', 'Moderately', 'Brains', 'Pizza']), ['', 'Pizza', 'Brains', 'Moderately']);
