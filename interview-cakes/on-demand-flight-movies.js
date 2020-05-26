ec9b416e0e0c8691966ffe20f518fbce47ac574c
casdidier <cheung.didier@gmail.com>
2020-05-26 23:11:21 +0200

    with bonus

--- interview-cakes/on-demand-flight-movies.js
+++ interview-cakes/on-demand-flight-movies.js
@@ -1,16 +1,31 @@
 // https://www.interviewcake.com/question/javascript/inflight-entertainment?course=fc1&section=hashing-and-hash-tables
 
 function canTwoMoviesFillFlight(movieLengths, flightLength) {
+  // convert movies length to min
+  const movieLengthsMin = movieLengths.map(x => x * 60);
+
   // Movie lengths we've seen so far
   const movieLengthsSeen = new Set();
 
-  for (let i = 0; i < movieLengths.length; i++) {
-    const firstMovieLength = movieLengths[i];
+  for (let i = 0; i < movieLengthsMin.length; i++) {
+    const firstMovieLength = movieLengthsMin[i];
 
     const matchingSecondMovieLength = flightLength - firstMovieLength;
-    if (movieLengthsSeen.has(matchingSecondMovieLength)) {
-      return true;
+
+    // short flight
+    if (movieLengthsMin.every(movie => movie > flightLength)) { return false; }
+
+    // check if within 20min there is a movie in the already seen
+    for (let j = 0; j < 20; j++) {
+      if (movieLengthsSeen.has(matchingSecondMovieLength + i)) {
+        return true;
+      }
+
+      if (movieLengthsSeen.has(matchingSecondMovieLength - i)) {
+        return true;
+      }
     }
+
 
     movieLengthsSeen.add(firstMovieLength);
   }
@@ -19,14 +34,6 @@ function canTwoMoviesFillFlight(movieLengths, flightLength) {
   return false;
 }
 
-// Tests
-function assertEquals(a, b, desc) {
-  if (a === b) {
-    console.log(`${desc} ... PASS`);
-  } else {
-    console.log(`${desc} ... FAIL: ${a} != ${b}`);
-  }
-}
 
 let desc = 'short flight';
 let actual = canTwoMoviesFillFlight([2, 4], 1);
@@ -66,4 +73,11 @@ assertEquals(actual, expected, desc);
 desc = 'no movies';
 actual = canTwoMoviesFillFlight([], 2);
 expected = false;
+assertEquals(actual, expected, desc);
+
+// BONUS
+// What if we wanted the movie lengths to sum to something close to the flight length (say, within 20 minutes)?
+desc = 'within 20min close to the flight';
+actual = canTwoMoviesFillFlight([120, 123, 140, 110], 235);
+expected = true;
 assertEquals(actual, expected, desc);
