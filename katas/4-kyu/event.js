30090f3f4c29b5c8fc62b603506374a6e86412fd
casdidier <cheung.didier@gmail.com>
2020-09-21 11:37:34 +0200

    other implementation of event

--- katas/4-kyu/event.js
+++ katas/4-kyu/event.js
@@ -17,3 +17,45 @@ class Event {
     this.handlers.forEach(f => f(...args));
   }
 }
+
+
+// other solutions
+
+function Event() {
+  var handlers = [];
+
+  this.subscribe = function (handler) {
+    handlers.indexOf(handler) === -1 && handlers.push(handler);
+  }
+
+  this.unsubscribe = function (handler, p) {
+    (p = handlers.indexOf(handler)) > -1 && handlers.splice(p, 1);
+  }
+
+  this.emit = function () {
+    var inArgs = arguments;
+
+    handlers.forEach(function (handler) {
+      handler.apply(this, inArgs);
+    });
+  }
+}
+
+
+class Event {
+  constructor() {
+    this.subscribers = new Set();
+  }
+
+  subscribe(func) {
+    this.subscribers.add(func);
+  }
+
+  unsubscribe(func) {
+    this.subscribers.delete(func);
+  }
+
+  emit(...args) {
+    this.subscribers.forEach(s => s(...args));
+  }
+}
