7e0d9edf38723f52436ea6c9e925eba0950c851c
casdidier <cheung.didier@gmail.com>
2020-09-23 17:05:32 +0200

    kata recycling 6th

---
+++ katas/6-kyu/recycle.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/5b6db1acb118141f6b000060/solutions/javascript
+
+function recycle(arr) {
+  const paper = []; const glass = []; const organic = []; const
+    plastic = [];
+
+  arr.forEach((obj) => {
+    if (obj.material === 'paper' || obj.secondMaterial === 'paper') paper.push(obj.type);
+    if (obj.material === 'glass' || obj.secondMaterial === 'glass') glass.push(obj.type);
+    if (obj.material === 'organic' || obj.secondMaterial === 'organic') organic.push(obj.type);
+    if (obj.material === 'plastic' || obj.secondMaterial === 'plastic') plastic.push(obj.type);
+  });
+
+  return [paper, glass, organic, plastic];
+}
+
+
+// reducer technique 
+const recycle = a => a.reduce((r, e) => {
+  r[rule[e.material]].push(e.type);
+  if (e.secondMaterial) r[rule[e.secondMaterial]].push(e.type);
+  return r;
+}, [[],[],[],[]]);
+
+const rule = {
+  paper: 0,
+  glass: 1,
+  organic: 2,
+  plastic: 3
+};
