55e3af7b890da8e34415a3058b0d562218eb65d9
casdidier <cheung.didier@gmail.com>
2020-10-04 15:19:07 +0200

    beautified

--- tech-interview-handbook/week1/productExceptSelf.js
+++ tech-interview-handbook/week1/productExceptSelf.js
@@ -18,29 +18,27 @@
 // };
 
 
-
 // 104 ms	45 MB
-var productExceptSelf = function(nums) {
-
-  let product = nums.reduce((cum, curr) => (cum*= curr), 1)
-  let arr = Array(nums.length).fill(product);
+const productExceptSelf = function (nums) {
+  const product = nums.reduce((cum, curr) => (cum *= curr), 1);
+  const arr = Array(nums.length).fill(product);
 
 
   // handle the cases with zero
   if (nums.filter(x => x === 0).length >= 2) return Array(nums.length).fill(0);
   if (nums.filter(x => x === 0).length === 1) {
-      let idxZero = nums.indexOf(0);
-      let productWithoutZero = nums.reduce((cum, curr, idx) => {
-          if (idx === idxZero) return cum;
-          return cum*curr;
-      }, 1)
+    const idxZero = nums.indexOf(0);
+    const productWithoutZero = nums.reduce((cum, curr, idx) => {
+      if (idx === idxZero) return cum;
+      return cum * curr;
+    }, 1);
 
-      let res = Array(nums.length).fill(0);
-      res[idxZero] = productWithoutZero;
-      return res;
-      }
+    const res = Array(nums.length).fill(0);
+    res[idxZero] = productWithoutZero;
+    return res;
+  }
 
   return arr.map((num, index) => {
-      return  num / nums[index];
-  })
+    return num / nums[index];
+  });
 };
